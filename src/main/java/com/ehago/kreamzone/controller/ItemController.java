87430352a235ac01f275c68eb8f216a545de94c5
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2021-07-19 13:09:24 +0900

    [#63] fix: CORS 허용 (#64)

--- src/main/java/com/ehago/kreamzone/controller/ItemController.java
+++ src/main/java/com/ehago/kreamzone/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.ehago.kreamzone.dto.response.item.ItemResponseDto;
 import com.ehago.kreamzone.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ItemController {
 
     private final ItemService itemService;
